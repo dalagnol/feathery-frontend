55c0e66a1e8f3a9a67269effe6fc4a1ccebae6a5
Júlia Klebanowycz <juliadak@icloud.com>
2019-12-20 15:15:40 -0300

    my first duck

---
+++ src/redux/ducks/user.ts
@@ -0,0 +1,26 @@
+export const Types = {
+  LOGOUT: "user/LOGOUT"
+};
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  loading: false,
+  error: null,
+  data: JSON.parse(localStorage.getItem("user"))
+};
+
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case Types.LOGOUT:
+      return { ...state, token: null, loading: false, error: null, data: null };
+    default:
+      return { ...state };
+  }
+};
+
+export const Creator = {
+    logout: () => async dispatch => {
+        localStorage.clear();
+        dispatch ({ type: Types.LOGOUT});
+    },
+};
