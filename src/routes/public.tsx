421162a46cccdb880bd3fa60f8721022c2fbd78d
Júlia Klebanowycz <juliadak@icloud.com>
2019-12-20 20:21:10 -0300

    publicRoutes setup

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-export default function public() {
-  return <div></div>;
-}
+import Views from "views";
+
+/**
+ * Defines the route for the splash page.
+ */
+export const splash = (): string => "/";
+
+/**
+ * Defines the route for the about page.
+ */
+export const about = (): string => "/about";
+
+/**
+ * Defines the route for the login page.
+ */
+export const login = (): string => "/login";
+
+/**
+ * Defines the route for the signIn page.
+ */
+export const signIn = (): string => "/signIn";
+
+export const publicRoutes = () => (
+  <Switch>
+    <Route exact path={splash()} render={() => <h1>Splash</h1>} />
+    <Route path={about()} render={() => null} />
+    <Route path={login()} render={() => null} />
+    <Route path={signIn()} render={() => null} />
+    <Redirect exact to={splash()} />
+  </Switch>
+);
+
+export default publicRoutes;
