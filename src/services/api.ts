2106844da8d6f3c6455fc41df4a40f876756cf8e
Júlia Klebanowycz <juliadak@icloud.com>
2019-12-20 19:52:27 -0300

    api setup

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,29 @@
-export default {};
+import axios from "axios";
+
+declare const process: any;
+
+const {
+  env: { HOST, PORT },
+} = process;
+
+const instance = axios.create({
+  baseURL: `${HOST}:${PORT}`,
+  timeout: 2000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+instance.interceptors.request.use(
+  request => {
+    const token = localStorage.getItem("token");
+    request.headers = {
+      Authorization: token || "",
+    };
+    return request;
+  },
+  error => Promise.reject(error)
+);
+
+export default instance;
