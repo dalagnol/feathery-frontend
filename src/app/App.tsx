d46917ee03d4d1dd2eaacca9a85d8afc85543215
Júlia Klebanowycz <juliadak@icloud.com>
2020-01-02 11:51:51 -0300

    Language Engine setup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import LanguageEngine from "locale";
 
 export default function App() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [init, setInit] = useState(true);
+  console.log(LanguageEngine);
+  return <div></div>;
 }
--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,3 +1,31 @@
-class LanguageEngine {}
+class LanguageEngine {
+  private Language: string = "en";
+  private Languages: Array<string> = ["en", "pt"];
+
+  constructor() {
+    this.getLangFromLS();
+    let path = window.location.pathname.split("/")[1];
+
+    if (this.Languages.includes(path)) {
+      this.Language = path;
+      localStorage.setItem("language", path);
+    } else {
+      this.getLangFromLS();
+    }
+  }
+
+  private getLangFromLS() {
+    this.Language = localStorage.getItem("language") || "en";
+  }
+
+  public get language(): string {
+    return this.Language;
+  }
+
+  public set language(lang: string) {
+    this.Language = lang;
+    localStorage.setItem("language", lang);
+  }
+}
 
 export default new LanguageEngine();
