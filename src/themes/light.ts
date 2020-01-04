417efc97a41becdbb198061f0b1b0f727412b0ef
Guilherme Moresco <guilherme.moresco@vortigo.com.br>
2020-01-04 16:19:16 -0300

    light+dark themes & index file

---
+++ src/themes/light.ts
@@ -0,0 +1,12 @@
+import { ITheme } from "interfaces/Theme";
+
+export const LightTheme: ITheme = {
+  primary: "white",
+  secondary: "#eee",
+  thirdiary: "#aaa",
+  text: "black",
+  shadowy: "#aaa",
+  blurry: "#eee",
+};
+
+export default LightTheme;
