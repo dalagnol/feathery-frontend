976b865f44b06bdea2710cd1708ea2004f41cebb
Guilherme Moresco <guilherme.moresco@vortigo.com.br>
2020-01-05 14:46:45 -0300

    strongly-typed Language Engine

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,12 +1,13 @@
+import { Language } from "./../interfaces/Locale";
+
 export default new (class LanguageEngine {
-  private Language: string = "en";
-  private Languages: Array<string> = ["en", "pt"];
+  private Language: Language = "en";
 
   constructor() {
     this.getLangFromLS();
-    let path = window.location.pathname.split("/")[1];
+    let path: string = window.location.pathname.split("/")[1];
 
-    if (this.Languages.includes(path)) {
+    if (path === "en" || path === "pt") {
       this.Language = path;
       localStorage.setItem("language", path);
     } else {
@@ -14,15 +15,18 @@ export default new (class LanguageEngine {
     }
   }
 
-  private getLangFromLS(): void {
-    this.Language = localStorage.getItem("language") || "en";
+  private getLangFromLS(): Language {
+    const L: Language | string | null =
+      localStorage.getItem("language") || "en";
+
+    return L === "en" || L === "pt" ? L : "en";
   }
 
-  public get language(): string {
+  public get language(): Language {
     return this.Language;
   }
 
-  public set language(lang: string) {
+  public set language(lang: Language) {
     this.Language = lang;
     localStorage.setItem("language", lang);
   }
