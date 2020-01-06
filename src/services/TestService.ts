a2f563226bfa590b11ec1d2b4c5bd6d602014b6c
Guilherme Moresco <34279299+morezco@users.noreply.github.com>
2020-01-06 09:20:06 -0300

    cheeky error message on TestService (#17)

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -2,7 +2,11 @@ import { api } from "services";
 
 export default new (class TestService {
   public async ping(): Promise<string> {
-    const { data } = await api.get("/ping");
-    return data;
+    try {
+      const { data } = await api.get("/ping");
+      return data;
+    } catch (oof) {
+      return "Unable to connect. Is dk-back running?";
+    }
   }
 })();
