81aed51c5d3e5d0526fafdac8b7f39bfd83a6e04
Júlia Klebanowycz <juliadak@icloud.com>
2020-01-06 22:43:13 -0300

    navbar component locale responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,9 +3,14 @@ import { withRouter } from "react-router-dom";
 
 import { Navbar as Element, Button } from "./styles";
 
+import Locale from "locale";
+import Dictionary from "./locale.json";
+
 import Theme from "themes";
 
 import Logo from "components/Logo/Logo";
+
+const { signup, signin } = Locale.use(Dictionary);
 
 function Navbar({ history }: any) {
   const SignUp = () => {
@@ -19,11 +24,11 @@ function Navbar({ history }: any) {
   return (
     <Element {...Theme.d}>
       <Button {...Theme.d} onClick={SignUp}>
-        Sign up
+        {signup}
       </Button>
       <Logo></Logo>
       <Button {...Theme.d} onClick={SignIn}>
-        Sign in
+        {signin}
       </Button>
     </Element>
   );
