870af7f160a61c120fbce7f5d7b2e95139893e1e
Guilherme Aquino Moresco <guilherme.moresco@dbccompany.com.br>
2020-01-09 15:32:17 -0300

    removal of test view "Adimo"

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,5 @@ import Landing from "./Landing/Landing";
 import About from "./About/About";
 import SignIn from "./SignIn/SignIn";
 import SignUp from "./SignUp/SignUp";
-import Adimo from "./Adimo/Adimo";
 
-export { Landing, About, SignIn, SignUp, Adimo };
+export { Landing, About, SignIn, SignUp };
