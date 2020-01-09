214f5b243c6054a600696643cf2bef4068a7ac2f
Guilherme Aquino Moresco <guilherme.moresco@dbccompany.com.br>
2020-01-09 15:07:36 -0300

    neater code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "styles/standards.css";
 import "styles/fonts.css";
 
-import PublicRoutes from "routes/public";
+import Public from "routes/public";
 
 import { Themed } from "themes";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Themed>
-        <PublicRoutes />
+        <Public />
       </Themed>
-    </BrowserRouter>
+    </Router>
   );
 }
