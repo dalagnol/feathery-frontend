1e203d02ca837abc074e7bf5457d4d745dc3119c
Júlia Klebanowycz <juliadak@icloud.com>
2020-01-09 19:27:40 -0300

    footer set to footer instead of div

--- src/components/Layout/Footer/styles.ts
+++ src/components/Layout/Footer/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { iPhone, iPad, Mac } from "styles/Screens";
 
-export const Footer = styled.div`
+export const Footer = styled.footer`
   display: flex;
   justify-content: space-between;
   align-items: center;
