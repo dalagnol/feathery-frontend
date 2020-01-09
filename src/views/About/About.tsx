232af984eb85845f9c9cd42ffe8ea569eeda5721
Júlia Klebanowycz <juliadak@icloud.com>
2020-01-09 16:17:42 -0300

    About view new style

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -7,7 +7,7 @@ import { Paragraph, Subheading } from "./styles";
 import Dictionary from "./locale.json";
 import Locale from "locale";
 
-import Theme from "themes";
+import { Themed } from "themes";
 
 //import Test from "services/TestService";
 
@@ -33,18 +33,24 @@ export default withRouter(function About({ history }) {
   //const ping = useService(Test.ping);
 
   return (
-    <Layout>
-      <Title>{aboutus}</Title>
-      <Paragraph {...Theme.d}>{aboutfeathery}</Paragraph>
-      <Subheading {...Theme.d}>{creators}</Subheading>
-      <Paragraph {...Theme.d}>{aboutcreators}</Paragraph>
-      <Developer photo={morezco} name={"Morezco"} description={aboutmorezco} />
-      <Developer
-        photo={dalagnol}
-        name={"Dalagnol"}
-        description={aboutdalagnol}
-      />
-      <Button onClick={Main}>{main}</Button>
-    </Layout>
+    <Themed>
+      <Layout>
+        <Title>{aboutus}</Title>
+        <Paragraph>{aboutfeathery}</Paragraph>
+        <Subheading>{creators}</Subheading>
+        <Paragraph>{aboutcreators}</Paragraph>
+        <Developer
+          photo={morezco}
+          name={"Morezco"}
+          description={aboutmorezco}
+        />
+        <Developer
+          photo={dalagnol}
+          name={"Dalagnol"}
+          description={aboutdalagnol}
+        />
+        <Button onClick={Main}>{main}</Button>
+      </Layout>
+    </Themed>
   );
 });
