316879c67122d12557e4684b6c5708ab960f4038
Júlia Klebanowycz <juliadak@icloud.com>
2020-01-09 17:07:04 -0300

    checkpoint

--- src/components/Layout/Background/Background.tsx
+++ src/components/Layout/Background/Background.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import { Background as Element } from "./styles";
 
-export default function Background({ children }: any) {
-  return <Element>{children}</Element>;
+export default function Background({ children, ...props }: any) {
+  return <Element {...props}>{children}</Element>;
 }
