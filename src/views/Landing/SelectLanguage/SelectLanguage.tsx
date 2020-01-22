683354964e5d728d1ed97c85304ab0ee51cee4a5
Júlia Klebanowycz <juliadak@icloud.com>
2020-01-22 19:32:31 -0300

    checkpoint

--- src/views/Landing/SelectLanguage/SelectLanguage.tsx
+++ src/views/Landing/SelectLanguage/SelectLanguage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRerender } from "shared/hooks";
 
 import { LangIcon, Container } from "./styles";
 
@@ -6,12 +7,20 @@ import { SelectButton } from "components";
 import { Option } from "components/SelectButton";
 
 export default function SelectLanguage() {
+  /*
+  const refresh = useRerender();
+  const changeLang = () => {
+    localStorage.setItem("language", "pt");
+    refresh();
+  };
+  */
+ 
   return (
     <Container>
       <LangIcon />
-      <SelectButton placeholder={"Select Language"}>
-        <Option>English</Option>
-        <Option>Portuguese</Option>
+      <SelectButton>
+        <Option value={"en"}>English</Option>
+        <Option value={"pt"}>Portuguese</Option>
       </SelectButton>
     </Container>
   );
