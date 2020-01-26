00044e7320b158a4e719fed79d4cc88acc8a1780
Guilherme Moresco <34279299+morezco@users.noreply.github.com>
2020-01-26 19:47:30 -0300

    Exp/sockets (#52)

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Socket from "contexts/Socket";
+
+import { Container, Input } from "./styles";
+
+export default function Chat({ room, sender }: any) {
+  const [history, setHistory]: any = useState([]);
+
+  const handler = (e: any, Server: any) => {
+    const {
+      keyCode,
+      target: { value },
+    } = e;
+
+    if (keyCode === 13) {
+      Server.emit("Message", room, { sender, value });
+    }
+  };
+
+  const events = {
+    Message: (message: any) => {
+      if (message.sender !== sender) {
+        setHistory([...history, message]);
+      }
+    },
+  };
+
+  return (
+    <Socket room={room} events={events}>
+      {({ Server }: any) => (
+        <>
+          <Container>
+            {history.map((message: any, index: number) => (
+              <h5 key={index}>{message.value}</h5>
+            ))}
+          </Container>
+          <Input onKeyDown={(e: any) => handler(e, Server)} />
+        </>
+      )}
+    </Socket>
+  );
+}
--- src/views/Nobody/Adimo/Adimo.tsx
+++ src/views/Nobody/Adimo/Adimo.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Themed } from "themes";
 
-import { Layout } from "components";
+import { Layout, Chat } from "components";
 
 export default function Adimo() {
   return (
     <Themed>
-      <Layout row around></Layout>
+      <Layout row around>
+        <Chat sender={"moresco"} room="Adimo" />
+        <Chat sender={"juba"} room="Adimo" />
+      </Layout>
     </Themed>
   );
 }
